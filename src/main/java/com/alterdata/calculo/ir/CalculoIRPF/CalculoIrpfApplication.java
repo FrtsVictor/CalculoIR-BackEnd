258f47b1e588894a78119c0d28d76a1ab0adcab7
FrtsVictor <victorfreitas.job@gmail.com>
2020-12-28 07:34:54 -0300

    Add Basic Structure

---
+++ src/main/java/com/alterdata/calculo/ir/CalculoIRPF/CalculoIrpfApplication.java
@@ -0,0 +1,13 @@
+package com.alterdata.calculo.ir.CalculoIRPF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoIrpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoIrpfApplication.class, args);
+	}
+
+}
