f738daf68dc32ac92d8b17daf24cc568ca5dbf09
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-07 23:51:07 -0300

    Fix imports

--- src/test/java/com/alterdata/calculo/ir/CalculoIRPF/User/UserControllerTest.java
+++ src/test/java/com/alterdata/calculo/ir/CalculoIRPF/User/UserControllerTest.java
@@ -9,14 +9,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @DisplayName("Teste com mokito sem a tag @WebMVC")
