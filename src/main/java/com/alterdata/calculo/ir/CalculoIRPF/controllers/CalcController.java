2e2578800561eab1b573d97417bcfcab3491f80e
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-11 23:20:27 -0300

    Added user validations & refactor methods

--- src/main/java/com/alterdata/calculo/ir/CalculoIRPF/controllers/CalcController.java
+++ src/main/java/com/alterdata/calculo/ir/CalculoIRPF/controllers/CalcController.java
@@ -1,6 +1,5 @@
 package com.alterdata.calculo.ir.CalculoIRPF.controllers;
 
-import com.alterdata.calculo.ir.CalculoIRPF.entity.User;
 import com.alterdata.calculo.ir.CalculoIRPF.exceptions.UserCVSInValitaionException;
 import com.alterdata.calculo.ir.CalculoIRPF.models.UserINSSIn;
 import com.alterdata.calculo.ir.CalculoIRPF.models.UserINSSOut;
@@ -10,7 +9,7 @@ import com.alterdata.calculo.ir.CalculoIRPF.modelsCVS.CalcUserIn;
 import com.alterdata.calculo.ir.CalculoIRPF.modelsCVS.CalcUserOut;
 import com.alterdata.calculo.ir.CalculoIRPF.services.calcAnualIRPF.CalcAliqAnualSimplesIRPFService;
 import com.alterdata.calculo.ir.CalculoIRPF.services.calcAnualIRPF.CalcIRSimplesService;
-import com.alterdata.calculo.ir.CalculoIRPF.services.calcINSS.CalcINSS;
+import com.alterdata.calculo.ir.CalculoIRPF.services.calcINSS.CalcINSSService;
 import com.alterdata.calculo.ir.CalculoIRPF.services.calcIRRF.CalcIRRFService;
 import com.alterdata.calculo.ir.CalculoIRPF.services.user.DefaultUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,59 +32,35 @@ public class CalcController {
     CalcAliqAnualSimplesIRPFService calcAliqAnualService;
 
     @Autowired
-    CalcINSS calcINSS;
+    CalcINSSService calcINSSService;
 
     @Autowired
     CalcIRRFService calcIRRFService;
 
     @PostMapping
     public CalcUserOut calcularIR(@Valid @RequestBody CalcUserIn usrIn) throws UserCVSInValitaionException {
-        userService.validateUserCsvIn(usrIn);
-
-        calcAliqAnualService.generateDeducaoAndAliquota2(usrIn);
-
+        userService.validateUser(usrIn);
+        calcAliqAnualService.generateAliquota(usrIn);
         calculoIR.calcularIR(calcAliqAnualService);
-
         CalcUserOut usrOut = calculoIR.generateCSVOut();
-        userService.CopyNameCpfNasc(usrIn,usrOut);
-
+        usrOut.setNome(usrIn.getNome());
         return usrOut;
     }
 
 
     @PostMapping("/inss")
     public UserINSSOut calcularINSS(@Valid @RequestBody UserINSSIn usrIn) throws UserCVSInValitaionException {
-        calcINSS.generateINSS(usrIn.getSalarioBruto());
+        userService.validateUserINSS(usrIn);
+        calcINSSService.generateINSS(usrIn.getSalarioBruto());
 
-        return UserINSSOut.builder()
-                .salarioBruto(usrIn.getSalarioBruto())
-                .nome(usrIn.getNome())
-                .parcelaADeduzir(calcINSS.getParcelaADeduzir())
-                .porcentagemAliquota(calcINSS.getPorcentagemAliquota())
-                .inss(calcINSS.getInss())
-                .deducaoAliquota(calcINSS.getDeducaoAliquota())
-                .build();
+        return calcINSSService.generateUserINSSOut(usrIn);
     }
 
     @PostMapping("/irrf")
     public UserIRRFOut generateIRRF(@Valid @RequestBody UserIRRFIn usrIn) throws UserCVSInValitaionException {
-        calcIRRFService.generateIRRF(usrIn);
+        userService.validateUserIRRF(usrIn);
 
-        return UserIRRFOut.builder()
-                .nome(usrIn.getNome())
-                .salarioBruto(usrIn.getSalarioBruto())
-                .INSS(calcINSS.generateINSS(usrIn.getSalarioBruto()))
-                .dependentes(usrIn.getDependentes())
-                .valorDependentes(calcIRRFService.getValorDependentes())
-                .pensaoAlimenticia(usrIn.getPensaoAlimenticia())
-                .valorTotalDescontos(calcIRRFService.getValorTotalDescontos())
-                .baseCalculo(calcIRRFService.getBaseCalculo())
-                .porcentagemAliquota(calcIRRFService.getPorcentagemAliquota())
-                .parcelaADeduzir(calcIRRFService.getParcelaADeduzir())
-                .IRRF(calcIRRFService.getIRRF())
-                .build();
+        return calcIRRFService.generateIRRF(usrIn);
     }
-
-
 
 }
