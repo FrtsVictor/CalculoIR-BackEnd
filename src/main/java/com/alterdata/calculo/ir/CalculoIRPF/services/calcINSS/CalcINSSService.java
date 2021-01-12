c61ac3eb22ee50b0300417aca2db45367f6c537c
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-11 22:59:24 -0300

    Added CacINSSService

---
+++ src/main/java/com/alterdata/calculo/ir/CalculoIRPF/services/calcINSS/CalcINSSService.java
@@ -0,0 +1,73 @@
+package com.alterdata.calculo.ir.CalculoIRPF.services.calcINSS;
+
+import com.alterdata.calculo.ir.CalculoIRPF.models.UserINSSIn;
+import com.alterdata.calculo.ir.CalculoIRPF.models.UserINSSOut;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.text.DecimalFormat;
+
+@Getter
+@Data
+@Service
+public class CalcINSSService implements BaseMensalAliquotaINSS {
+
+    private double parcelaADeduzir;
+    private double porcentagemAliquota;
+    private double inss;
+    private double deducaoAliquota;
+
+    private double DecimalFormater(double decimal) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        return Double.parseDouble(df.format(decimal));
+    }
+
+    public double generateINSS(double salarioBruto){
+        generateFaixaSalarial(salarioBruto);
+        this.deducaoAliquota = salarioBruto * ( this.porcentagemAliquota / 100);
+        double inss =  this.deducaoAliquota - this.parcelaADeduzir;
+
+        if(inss > tetoINSS){
+            inss = tetoINSS;
+        }
+        this.inss = DecimalFormater(inss);
+        return DecimalFormater(inss);
+    }
+
+    public void generateFaixaSalarial(double salarioBruto){
+        if(salarioBruto <= faixa1_valorFinal){
+            this.parcelaADeduzir = faixa1_parcelaADeduzir;
+            this.porcentagemAliquota = faixa1_aliquota;
+        }
+
+        if(salarioBruto > faixa1_valorFinal && salarioBruto <= faixa2_valorFinal){
+            this.parcelaADeduzir = faixa2_parcelaADeduzir;
+            this.porcentagemAliquota = faixa2_aliquota;
+        }
+
+        if(salarioBruto > faixa2_valorFinal && salarioBruto <= faixa3_valorFinal){
+            this.parcelaADeduzir = faixa3_parcelaADeduzir;
+            this.porcentagemAliquota = faixa3_aliquota;
+        }
+
+        if(salarioBruto >= faixa4_valorInicial ){
+            this.parcelaADeduzir = faixa4_parcelaADeduzir;
+            this.porcentagemAliquota = faixa4_aliquota;
+        }
+    }
+
+    public UserINSSOut generateUserINSSOut(UserINSSIn user){
+        return UserINSSOut.builder()
+                .salarioBruto(user.getSalarioBruto())
+                .nome(user.getNome())
+                .parcelaADeduzir(this.getParcelaADeduzir())
+                .porcentagemAliquota(this.getPorcentagemAliquota())
+                .inss(this.getInss())
+                .deducaoAliquota(this.getDeducaoAliquota())
+                .build();
+    }
+
+}
+
+
