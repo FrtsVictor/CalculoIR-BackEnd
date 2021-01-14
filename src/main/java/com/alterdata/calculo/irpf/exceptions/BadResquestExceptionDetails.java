af4c8d705d5767883ab9d52af5703954743f955c
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-13 23:47:13 -0300

    Added BadRequestExceptionDetails

---
+++ src/main/java/com/alterdata/calculo/irpf/exceptions/BadResquestExceptionDetails.java
@@ -0,0 +1,12 @@
+package com.alterdata.calculo.irpf.exceptions;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class BadResquestExceptionDetails extends ExceptionDetails{
+
+
+
+}
