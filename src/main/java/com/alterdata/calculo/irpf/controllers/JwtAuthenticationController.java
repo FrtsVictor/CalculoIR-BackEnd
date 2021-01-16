3810abd49f0f8af059aa981fc753dd5bbbe346a9
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-16 08:18:31 -0300

    Refactor package name

--- src/main/java/com/alterdata/calculo/irpf/controllers/JwtAuthenticationController.java
+++ src/main/java/com/alterdata/calculo/irpf/controllers/JwtAuthenticationController.java
@@ -1,22 +1,15 @@
 package com.alterdata.calculo.irpf.controllers;
 
-import com.alterdata.calculo.irpf.config.JwtTokenUtil;
-import com.alterdata.calculo.irpf.dto.UserDTO;
-import com.alterdata.calculo.irpf.models.JwtRequest;
-import com.alterdata.calculo.irpf.models.JwtResponse;
-import com.alterdata.calculo.irpf.models.User;
-import com.alterdata.calculo.irpf.repositories.UserRepository;
-import com.alterdata.calculo.irpf.services.JwtUserDetailsService;
-import com.alterdata.calculo.irpf.services.user.UserRepositoryServer;
+import com.alterdata.calculo.irpf.models.account.User;
+import com.alterdata.calculo.irpf.models.account.UserRequest;
+import com.alterdata.calculo.irpf.models.jwt.JwtRequest;
+import com.alterdata.calculo.irpf.models.jwt.JwtResponse;
+import com.alterdata.calculo.irpf.services.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,32 +19,45 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class JwtAuthenticationController {
 
-	final AuthenticationManager authenticationManager;
-	final private JwtTokenUtil jwtTokenUtil;
-	final JwtUserDetailsService userDetailsService;
-	final UserRepositoryServer userService;
+//	final AuthenticationManager authenticationManager;
+
+	final UserService userService;
 
 	@PostMapping("/authenticate")
 	public ResponseEntity<JwtResponse> createAuthenticationToken(@Valid @RequestBody JwtRequest authenticationRequest) throws Exception {
-		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-		final UserDetails userDetails = userDetailsService
-				.loadUserByUsername(authenticationRequest.getUsername());
-		final String token = jwtTokenUtil.generateToken(userDetails);
-		return ResponseEntity.ok(new JwtResponse(token));
+		userService.authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+		return ResponseEntity.ok(userService.generateToken(authenticationRequest));
 	}
 
 	@PostMapping("/register")
-	public ResponseEntity<User> saveUser(@Valid @RequestBody UserDTO user) throws Exception {
-		return ResponseEntity.ok(userService.save(user));
+	public ResponseEntity<User> saveUser(@Valid @RequestBody UserRequest user) throws Exception {
+		return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
 	}
 
-	private void authenticate(String username, String password) throws Exception {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
-		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
-		}
+	@GetMapping("/all")
+	public ResponseEntity<Page<User>> getALl(Pageable pageable){
+		return ResponseEntity.ok(userService.listALl(pageable));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id){
+		return ResponseEntity.ok(userService.findByIdOrThrowBadRequestException(id));
+	}
+
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByIdUsername(@PathVariable String username){
+		return ResponseEntity.ok(userService.findByUserNameOrThrowsBadRequestException(username));
+	}
+
+	@PutMapping
+	public ResponseEntity<Void> updateUser(@RequestBody UserRequest user){
+		userService.replace(user);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@DeleteMapping
+	public ResponseEntity<Void> deleteUser(@RequestParam Integer id){
+		userService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
