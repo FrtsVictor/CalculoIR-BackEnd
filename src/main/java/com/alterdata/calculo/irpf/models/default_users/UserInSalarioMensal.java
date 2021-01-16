14116a4bb1343017c77e9cf38b62c40f9d2c9350
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-16 08:22:37 -0300

    Added UserInSalarioMensal

---
+++ src/main/java/com/alterdata/calculo/irpf/models/default_users/UserInSalarioMensal.java
@@ -0,0 +1,26 @@
+package com.alterdata.calculo.irpf.models.default_users;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public abstract class UserInSalarioMensal {
+
+    @NotEmpty
+    @Size(min = 4, max = 80)
+    protected String nome;
+    protected double salarioMensalBruto;
+
+    protected boolean validateSalarioBruto(UserInSalarioMensal usrIn) {
+        if (usrIn.getSalarioMensalBruto() < 1) {
+            return false;
+        }
+        return true;
+    }
+
+}
