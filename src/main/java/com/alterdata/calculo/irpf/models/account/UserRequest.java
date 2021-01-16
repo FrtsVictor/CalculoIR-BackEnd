2552e0d709bead257746e31bae868dd12c3d78c5
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-16 08:22:20 -0300

    Refactor user Dto to Request

---
+++ src/main/java/com/alterdata/calculo/irpf/models/account/UserRequest.java
@@ -0,0 +1,28 @@
+package com.alterdata.calculo.irpf.models.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRequest {
+
+	@NotEmpty
+	@Size(min = 5, max = 30)
+	private String username;
+	@NotEmpty
+	@Size(min = 5, max = 50)
+	private String password;
+	@NotEmpty
+	@Size(min = 3, max = 80)
+	private String nome;
+
+}
