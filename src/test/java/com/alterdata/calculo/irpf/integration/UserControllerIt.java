3f8d0841abb97f44b155d3ba13f70c6d83857ac2
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-20 01:56:18 -0300

    Fixed UserCOntrollerIT routes

--- src/test/java/com/alterdata/calculo/irpf/integration/UserControllerIt.java
+++ src/test/java/com/alterdata/calculo/irpf/integration/UserControllerIt.java
@@ -4,12 +4,11 @@ import com.alterdata.calculo.irpf.models.account.User;
 import com.alterdata.calculo.irpf.models.account.UserRequest;
 import com.alterdata.calculo.irpf.models.jwt.JwtRequest;
 import com.alterdata.calculo.irpf.models.jwt.JwtResponse;
-import com.alterdata.calculo.irpf.repositories.UserRepository;
 import com.alterdata.calculo.irpf.services.user.UserService;
 import com.alterdata.calculo.irpf.util.UserCreator;
 import com.alterdata.calculo.irpf.util.UserRequestCreator;
-import com.alterdata.calculo.irpf.wrapper.PageableResponse;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +16,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Collections;
-import java.util.Map;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
@@ -37,17 +34,36 @@ class UserControllerIT {
     private int port;
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
     UserService userService;
 
-    @Test
-    @DisplayName("save deve retornar user para usuario valido quando sucesso")
-    void deve_retornar_user_quando_salvo_com_sucesso() throws Exception {
-        UserRequest userRequest = UserRequestCreator.createUserRequestBody();
+    HttpHeaders httpHeaders;
 
-        ResponseEntity<User> userResponseEntity = testRestTemplate.postForEntity("/register", userRequest, User.class);
+    @BeforeEach
+    void generateToken(){
+        User savedUser = userService.save(UserRequestCreator.createUserRequestBody());
+        JwtRequest loginRequest = new JwtRequest(savedUser.getUsername(), "123321");
+
+        ResponseEntity<JwtResponse> JWTResponse = testRestTemplate.postForEntity("/v1/authenticate",
+                loginRequest, JwtResponse.class);
+        String token = JWTResponse.getBody().getToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer "+ token);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        this.httpHeaders = new HttpHeaders(headers);
+    }
+
+    @Test
+    @DisplayName("save deve retornar usuario salvo quando sucesso")
+    void save_deve_retornar_user_quando_salvo_com_sucesso() throws Exception {
+        UserRequest userToBeSaved = new UserRequest();
+        userToBeSaved.setNome("Test2");
+        userToBeSaved.setUsername("testUsername2");
+        userToBeSaved.setPassword("testes");
+
+        ResponseEntity<User> userResponseEntity = testRestTemplate.postForEntity("/v1/register", userToBeSaved, User.class);
 
         Assertions.assertThat(userResponseEntity).isNotNull();
         Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
@@ -57,46 +73,32 @@ class UserControllerIT {
 
 
     @Test
-    @DisplayName("findUserByUsername deve retornar usuario quando sucesso")
-    void deve_retornar_usuario_por_username_quando_sucesso() {
-    User savedUser = userService.save(UserRequestCreator.createUserRequestBody());
-    JwtRequest loginRequest = new JwtRequest(savedUser.getUsername(), "123321");
+    @DisplayName("findUserByUsername deve retornar usuario por username quando sucesso")
+    void findUserByUsername_deve_retornar_usuario_por_username_quando_sucesso() {
+        User savedBeforeEach = UserCreator.createValidUser();
 
-        ResponseEntity<JwtResponse> JWTResponse = testRestTemplate.postForEntity("/authenticate",
-               loginRequest, JwtResponse.class);
-        String token = JWTResponse.getBody().getToken();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer "+ token);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity request = new HttpEntity(headers);
+        HttpEntity request = new HttpEntity(this.httpHeaders);
 
         ResponseEntity<User> userResponseEntity = testRestTemplate.exchange(
-                "/users/username/{username}",
+                "/v1/users/username/{username}",
                 HttpMethod.GET,
                 request,
                 User.class,
-                savedUser.getUsername()
+                savedBeforeEach.getUsername()
         );
 
         Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(userResponseEntity.getBody().getUsername()).isEqualTo(savedUser.getUsername());
+        Assertions.assertThat(userResponseEntity.getBody().getUsername()).isEqualTo(savedBeforeEach.getUsername());
     }
 
 
     @Test
-    @DisplayName("findUserByUsername deve retornar status 401 unauthorized para rota sem token")
-    void deve_retornar_401_unauthorized_quando_sem_token() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity request = new HttpEntity(headers);
+    @DisplayName("findUserByUsername deve retornar status 401 unauthorized para token invalido")
+    void findUserByUsername_deve_retornar_401_unauthorized_quando_sem_token() {
+        HttpEntity request = new HttpEntity(new HttpHeaders());
 
         ResponseEntity<User> userResponseEntity = testRestTemplate.exchange(
-                "/users/username/{username}",
+                "/v1/users/username/{username}",
                 HttpMethod.GET,
                 request,
                 User.class,
@@ -107,48 +109,32 @@ class UserControllerIT {
         Assertions.assertThat(userResponseEntity.getBody().getUsername()).isNull();
     }
 
-
-
     @Test
     @DisplayName("findById deve retornar user por ID quando ID valido")
-    void deve_retornar_user_por_id_quando_valido() {
-        User savedUser = userService.save(UserRequestCreator.createUserRequestBody());
-        JwtRequest loginRequest = new JwtRequest(savedUser.getUsername(), "123321");
+    void findById_deve_retornar_user_por_id_quando_valido() {
+        User savedBeforeEach = UserCreator.createValidUser();
 
-        ResponseEntity<JwtResponse> JWTResponse = testRestTemplate.postForEntity("/authenticate",
-                loginRequest, JwtResponse.class);
-        String token = JWTResponse.getBody().getToken();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer "+ token);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity request = new HttpEntity(headers);
+        HttpEntity request = new HttpEntity(this.httpHeaders);
 
         ResponseEntity<User> userResponseEntity = testRestTemplate.exchange(
-                "/users/id/{id}",
+                "/v1/users/id/{id}",
                 HttpMethod.GET,
                 request,
                 User.class,
-                savedUser.getId()
+                savedBeforeEach.getId()
         );
-        Assertions.assertThat(userResponseEntity.getBody().getId()).isNotNull().isEqualTo(savedUser.getId());
+        Assertions.assertThat(userResponseEntity.getBody().getId()).isNotNull().isEqualTo(savedBeforeEach.getId());
         Assertions.assertThat(userResponseEntity.getStatusCode()).isNotNull().isEqualTo(HttpStatus.OK);
     }
 
 
     @Test
-    @DisplayName("findById deve retornar status 401 unauthorized para rota sem token")
-    void deve_retornar_401_unauthorized_para_rota_sem_token() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity request = new HttpEntity(headers);
+    @DisplayName("findById deve retornar status 401 unauthorized para token invalido")
+    void findById_deve_retornar_401_unauthorized_para_rota_sem_token() {
+        HttpEntity request = new HttpEntity(new HttpHeaders());
 
         ResponseEntity<User> userResponseEntity = testRestTemplate.exchange(
-                "/users/id/{id}",
+                "/v1/users/id/{id}",
                 HttpMethod.GET,
                 request,
                 User.class,
@@ -158,63 +144,63 @@ class UserControllerIT {
         Assertions.assertThat(userResponseEntity.getStatusCode()).isNotNull().isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
+    @Test
+    @DisplayName("replace deve atualizar user quando sucesso")
+    void replace_deve_atualizar_user_quando_sucesso() {
+        UserRequest validUser = new UserRequest("testUsername3", "123456","Test3");
+        User userToBeUpdated = userService.save(validUser);
 
+        HttpEntity request = new HttpEntity(userToBeUpdated, this.httpHeaders);
 
-//    @Test
-//    @DisplayName("replace deve atualizar user quando sucesso")
-//    void deve_atualizar_user_quando_sucesso() {
-//        User userToBeUpdated = userService.save(UserRequestCreator.createUserRequestBody());
-//        JwtRequest loginRequest = new JwtRequest(userToBeUpdated.getUsername(), "123321");
-//
-//        ResponseEntity<JwtResponse> JWTResponse = testRestTemplate.postForEntity("/authenticate",
-//                loginRequest, JwtResponse.class);
-//        String token = JWTResponse.getBody().getToken();
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.set("Authorization", "Bearer "+ token);
-//        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-//
-//        HttpEntity request = new HttpEntity(headers);
-//
-//        userToBeUpdated.setNome("Freitas");
-//
-//        testRestTemplate.put("/users/{id}",
-//                request,
-//                Void.class,
-//                userToBeUpdated);
-//
-//        ResponseEntity<Void> userResponseEntity = testRestTemplate.exchange(
-//                "/users",
-//                HttpMethod.PUT,
-//                request,
-//                Void.class,
-//                userToBeUpdated
-//        );
-//
-//        Assertions.assertThat(userResponseEntity).isNotNull();
-//        Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
-//    }
+        userToBeUpdated.setNome("Freitas");
+
+        ResponseEntity<Void> userResponseEntity = testRestTemplate.exchange(
+                "/v1/users",
+                HttpMethod.PUT,
+                request,
+                Void.class
+        );
+
+        Assertions.assertThat(userResponseEntity).isNotNull();
+        Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    @Test
+    @DisplayName("replace deve lancar status 401 Unauthorized para token invalido")
+    void replace_deve_lancar_status_401_unauthorized_para_token_invalido() {
+        UserRequest validUser = new UserRequest("testUsername3", "123456","Test3");
+        User userToBeUpdated = userService.save(validUser);
+
+        HttpEntity request = new HttpEntity(userToBeUpdated, this.httpHeaders);
+
+        userToBeUpdated.setNome("Freitas");
+
+        ResponseEntity<Void> userResponseEntity = testRestTemplate.exchange(
+                "/v1/users",
+                HttpMethod.PUT,
+                request,
+                Void.class
+        );
+
+        Assertions.assertThat(userResponseEntity).isNotNull();
+        Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
 
     @Test
     @DisplayName("delete deve remover user quando sucesso")
-    void deve_deletar_user_quando_sucesso() {
-        User userSaved = userService.save(UserRequestCreator.createUserRequestBody());
-        JwtRequest loginRequest = new JwtRequest(userSaved.getUsername(), "123321");
+    void delete_deve_deletar_user_quando_sucesso() {
+        UserRequest userToBeSaved = new UserRequest();
+        userToBeSaved.setNome("Test2");
+        userToBeSaved.setUsername("testUsername2");
+        userToBeSaved.setPassword("testes");
 
-        ResponseEntity<JwtResponse> JWTResponse = testRestTemplate.postForEntity("/authenticate",
-                loginRequest, JwtResponse.class);
-        String token = JWTResponse.getBody().getToken();
+        User userSaved = userService.save(userToBeSaved);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer "+ token);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity request = new HttpEntity(headers);
+        HttpEntity request = new HttpEntity(httpHeaders);
 
         ResponseEntity<Void> userResponseEntity = testRestTemplate.exchange(
-                "/users/{id}",
+                "/v1/users/{id}",
                 HttpMethod.DELETE,
                 request,
                 Void.class,
@@ -224,18 +210,13 @@ class UserControllerIT {
         Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 
-
     @Test
-    @DisplayName("delete deve retornar status 401 unauthorized para rota sem token")
+    @DisplayName("delete deve retornar status 401 unauthorized para token invalido")
     void deve_retornar_status401_quando_para_rota_sem_token() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity request = new HttpEntity(headers);
+        HttpEntity request = new HttpEntity(new HttpHeaders());
 
         ResponseEntity<Void> userResponseEntity = testRestTemplate.exchange(
-                "/users/{id}",
+                "/v1/users/{id}",
                 HttpMethod.DELETE,
                 request,
                 Void.class,
@@ -243,6 +224,5 @@ class UserControllerIT {
         );
         Assertions.assertThat(userResponseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
-
 
 }
