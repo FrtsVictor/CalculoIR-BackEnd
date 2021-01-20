c1285950fee97ea7cdefbd3c69ccbe2657f4b7cb
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-20 01:49:50 -0300

    Added data anotation

--- src/main/java/com/alterdata/calculo/irpf/models/jwt/JwtRequest.java
+++ src/main/java/com/alterdata/calculo/irpf/models/jwt/JwtRequest.java
@@ -1,21 +1,21 @@
 package com.alterdata.calculo.irpf.models.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtRequest implements Serializable {
 
     private static final long serialVersionUID = 5926468583005150707L;
 
     private String username;
     private String password;
-
-    public JwtRequest() {
-    }
-
-    public JwtRequest(String username, String password) {
-        this.setUsername(username);
-        this.setPassword(password);
-    }
 
     public String getUsername() {
         return this.username;
